55ebb27c3e98c26f70ee2876c4cbf62e21d8b1f0
Thomas Grout <tomgrout65@gmail.com>
2025-10-03 16:39:15 +0100

    add files

---
+++ app/projects/projectsData.ts
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    name: "University Projects",
+    items: [
+      { name: "Home Device Manager", slug: "home-device-mgr", description: "Console based home Smart Device manager in C++." },
+      { name: "Asset Manager", slug: "asset-manager", description: "Worked with Autodesk to implement a 3D asset management system." },
+    ],
+  },
+  {
+    name: "Personal Projects",
+    items: [
+      { name: "Portfolio Site", slug: "portfolio", description: "Personal site built with Next.js." },
+      { name: "Company website, GBP", slug: "gbp-site", description: "Vue.js + vite company website, with powerful three.js features." },
+    ],
+  },
+];
+
+export default projectsData;  
